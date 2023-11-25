75ba2060ebcefb60af821c769b93bfa28475537f
Rabiul Mannan Sajed <rabiulmannansajed@gmail.com>
2023-11-26 04:34:33 +0600

    update

--- src/Pages/JobInfo/JobInfoCart/JobInfoCart.jsx
+++ src/Pages/JobInfo/JobInfoCart/JobInfoCart.jsx
@@ -1,3 +1,12 @@
+import {
+  faCalendarDay,
+  faHourglass1,
+  faLocationDot,
+  faMoneyBill1,
+  faPlayCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const JobInfoCart = ({ dataOfJob }) => {
   const {
     companyName,
@@ -15,7 +24,89 @@ const JobInfoCart = ({ dataOfJob }) => {
   } = dataOfJob;
   return (
     <div>
-      <h1>{companyName}</h1>
+      <h1 className="text-2xl font-bold text-center m-5">{jobTitle}</h1>
+
+      {/* for name and logo */}
+      <div className="flex justify-between ">
+        <div className="font-semibold">
+          <h1 className="mt-3 mb-3">{jobTitle}</h1>
+          <p>{companyName}</p>
+        </div>
+        <div>
+          <img src={companyLogo} alt="" />
+        </div>
+      </div>
+      <p className="mt-3  font-semibold">
+        {" "}
+        <FontAwesomeIcon icon={faLocationDot} className="mr-1  " />
+        {workFrom}
+      </p>
+
+      {/* this is  for $ start date EXPERIENCE  make them flex */}
+      <div className="flex justify-between mt-5 mb-5 w-3/4">
+        <div>
+          <h2>
+            <FontAwesomeIcon icon={faPlayCircle} className="mr-1" />
+            Start Date
+          </h2>
+          <p> {startDate}</p>
+        </div>
+        <div>
+          <h1>
+            {" "}
+            <FontAwesomeIcon icon={faMoneyBill1} className="mr-1" />
+            CTC(ANNUAL)
+          </h1>
+          <p>$ {stipend}</p>
+        </div>
+        <div>
+          <h1>
+            {" "}
+            <FontAwesomeIcon icon={faCalendarDay} className="mr-1" />
+            DURATION
+          </h1>
+          <p>$ {duration}</p>
+        </div>
+        <div>
+          <h1>
+            {" "}
+            <FontAwesomeIcon icon={faHourglass1} className="mr-1" />
+            EXPERIENCE
+          </h1>
+          <p>{experience}</p>
+        </div>
+      </div>
+      <div>
+        <h1>Skill(s) required</h1>
+
+        <div>
+          {Array.isArray(skill) && skill.length > 0 ? (
+            <div className="flex justify-between w-2/4 gap-1 m-3 font-semibold text-gray-600">
+              {skill.map((s, index) => (
+                <div key={index}>{s}</div>
+              ))}
+            </div>
+          ) : (
+            <p>No skills specified</p>
+          )}
+        </div>
+      </div>
+
+      {/* part of how can apply  */}
+      <div>
+        <h1>Who Can Apply</h1>
+        {Array.isArray(whoApply) && whoApply.length > 0 ? (
+          <div className="w-3/4 m-3 font-semibold text-gray-600">
+            {whoApply.map((s, index) => (
+              <div key={index} className="mt-3">
+                {index + 1}. {s}
+              </div>
+            ))}{" "}
+          </div>
+        ) : (
+          <p>No skills specified</p>
+        )}
+      </div>
     </div>
   );
 };
